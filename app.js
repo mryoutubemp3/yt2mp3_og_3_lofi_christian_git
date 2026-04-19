92ff1186b02926d72eec13dd79439e5ad3bfc9ad
mryoutubemp3 <mryoutubemp3@gmail.com>
2026-04-19 18:24:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.use('/downloads', express.static(path.join(__dirname,'downloads')));
 
 //////////////////////////////////////////////////////////
